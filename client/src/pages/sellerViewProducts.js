1914d3d35bdd1dbd617d89873277d63e7e48114e
yutakaki <takaki.yu.dev@gmail.com>
2021-05-26 23:43:38 +0900

    added seller view filter form

--- client/src/pages/sellerViewProducts.js
+++ client/src/pages/sellerViewProducts.js
@@ -3,10 +3,38 @@ import React from 'react'
 const ViewProducts = () => {
     return (
         <div className='viewSellerProducts'>
-            <div className='filter'>
-                <input type='text' />
+            <form className='filter'>
+                <div className='filterInputContainer'>
+                    <div className='filterSearchProduct'>
+                        <label htmlFor='productName'>Product Name</label>
+                        <input id='productName' type='text' />
+                    </div>
+                    <div className='filterProductCategory'>
+                        <label htmlFor='category'>Category</label>
+                        <select id='category'>
 
-            </div>
+
+                        </select>
+                    </div>
+                    <div className='filterSearchStock'>
+                        <label>Stock</label>
+                        <input type='number' />
+                         - 
+                        <input type='number' />
+                    </div>
+                    <div className='filterSearchPrice'>
+                        <label>Price</label>
+                        <input type='number' />
+                         - 
+                        <input type='number' />
+                    </div>
+                </div>
+                <div className='filterButtonContainer'>
+                    <input type='submit'/>
+                    <button>Reset</button>
+                </div>
+
+            </form>
             <div className='productContainer'>
 
             </div>
