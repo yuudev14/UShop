ac9b4a10293b63a489720d21f11247a50e09f0e4
yutakaki <takaki.yu.dev@gmail.com>
2021-09-07 22:38:42 +0900

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const path = require("path");
 const app = express();
 
 
 const port = process.env.PORT || 8000;
-
+app.use(cors());
 app.use(express.json({limit: '50mb', extended : true}));
 app.use(express.urlencoded({limit: '50mb', extended : true}));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //AuthenticationRoute
 app.use('/auth', require('./routes/authentication'));
